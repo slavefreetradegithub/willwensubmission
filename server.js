b683347a8b8f2bdd510724b712239819d522ba8b
Emily <emilykim@mit.edu>
2018-02-10 09:10:27 -0500

    process data on server

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var express = require("express");
 var path = require("path")
 var app = express();
+var bodyParser = require("body-parser"); //handles form submission data from post
 //Server anything in the webpage directory
 app.use(express.static(path.join(__dirname ,'webpage/')))
-// app.get("/light_on_max", (req, res) => {
-// });
+app.use(bodyParser.urlencoded());
+app.post("/data", (req, res) => { //access body of post request
 
-app.listen(9000, () => console.log('Example app listening on port 9000!'))
+//make variables for each data point (in form.html, name="XYZ" --> var XYZ=req.body.XYZ)
+    var optionsRadios=req.body.optionsRadios;
+
+
+//do what u want w data here, before res.end
+    console.log(optionsRadios);
+
+
+    res.end("data!") //ends ur processing
+
+
+});
+
+app.listen(9000, () => console.log('Slavefreetrade on port 9000!'))
